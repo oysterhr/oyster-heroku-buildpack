bf4e8110994cc307b37413b9f0a567ac4b515ea3
Francisco Lala <franciscofjlala@gmail.com>
2023-06-01 11:28:36 +0200

    Some extra tweaks

--- bin/compile.sh
+++ bin/compile.sh
@@ -8,40 +8,51 @@ BUILD_DIR=$1
 ENV_DIR=$3
 
 # Get env variables
-ROLE_ARN=$(cat "$ENV_DIR/ROLE_ARN")
-ID_TOKEN=$(cat "$ENV_DIR/ID_TOKEN")
-S3_BUCKET_NAME=$(cat "$ENV_DIR/S3_BUCKET_NAME")
+ENV_ROLE_ARN=$(cat "$ENV_DIR/ROLE_ARN")
+ENV_S3_BUCKET_NAME=$(cat "$ENV_DIR/S3_BUCKET_NAME")
+ENV_AWS_ACCESS_KEY_ID=$(cat "$ENV_DIR/AWS_ACCESS_KEY_ID")
+ENV_AWS_SECRET_ACCESS_KEY=$(cat "$ENV_DIR/AWS_SECRET_ACCESS_KEY")
 
-if [ -z "$ROLE_ARN" ]; then
+# Set env variables for aws cli
+export AWS_ACCESS_KEY_ID="$ENV_AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$ENV_AWS_SECRET_ACCESS_KEY"
+
+if [ -z "$ENV_AWS_ACCESS_KEY_ID" ]; then
+  echo "Skipping AWS sync. AWS_ACCESS_KEY_ID is not set"
+  exit 0
+fi
+
+if [ -z "$ENV_AWS_SECRET_ACCESS_KEY" ]; then
+  echo "Skipping AWS sync. AWS_SECRET_ACCESS_KEY is not set"
+  exit 0
+fi
+
+if [ -z "$ENV_ROLE_ARN" ]; then
   echo "Skipping AWS sync. ROLE_ARN is not set"
   exit 0
 fi
 
-if [ -z "$ID_TOKEN" ]; then
-  echo "Skipping AWS sync. ID_TOKEN is not set"
-  exit 0
-fi
 
-if [ -z "$S3_BUCKET_NAME" ]; then
+if [ -z "$ENV_S3_BUCKET_NAME" ]; then
   echo "Skipping AWS sync. S3_BUCKET_NAME is not set"
   exit 0
 fi
 
 # Assume role
-STS=($(aws sts assume-role-with-web-identity
-      --role-arn ${ROLE_ARN}
-      --role-session-name "${HEROKU_APP_NAME}-${HEROKU_RELEASE_VERSION}"
-      --web-identity-token $ID_TOKEN
-      --duration-seconds 3600
-      --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]'
-      --output text))
+STS=($(aws sts assume-role --role-arn ${ENV_ROLE_ARN} --role-session-name "${HEROKU_APP_NAME}-${HEROKU_RELEASE_VERSION}" --duration-seconds 3600 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text))
+
+# Unset env variables for aws cli
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_ACCESS_KEY_ID
+
+# Set new temporary env variables for aws cli with permissions from assumed role
 export AWS_ACCESS_KEY_ID="${STS[0]}"
 export AWS_SECRET_ACCESS_KEY="${STS[1]}"
 export AWS_SESSION_TOKEN="${STS[2]}"
 
 # Sync bucket
 PUBLIC_FOLDER="$BUILD_DIR/public"
-aws s3 sync $PUBLIC_FOLDER s3://$S3_BUCKET_NAME
+aws s3 sync $PUBLIC_FOLDER s3://$ENV_S3_BUCKET_NAME
 
 # Invalidate cache
 # We need to invalidate the cache for manifest.json and manifest-assets.json
